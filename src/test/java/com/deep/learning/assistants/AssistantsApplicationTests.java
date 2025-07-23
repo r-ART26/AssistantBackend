03e3fa0b997e681a4cfff22b1827488b47519425
r-ART26 <robertortups@gmail.com>
2025-07-23 13:53:28 -0500

    initial commit

---
+++ src/test/java/com/deep/learning/assistants/AssistantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deep.learning.assistants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssistantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
